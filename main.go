7277b7f75ff6e31427f93fa4a1ce0c586cbeb9d9
ioikonoma <iosifoikonomakis@gmail.com>
2026-06-11 18:07:25 +0300

    phase0 and phase1

--- main.go
+++ main.go
@@ -1,27 +1,27 @@
 package main
 
 import (
-    "fmt"
-    "io"
-    "os"
+	"fmt"
+	"io"
+	"os"
 )
 
 func main() {
-    if len(os.Args) < 2 {
-        fmt.Fprintln(os.Stderr, "usage: lem-in <input-file>")
-        os.Exit(1)
-    }
+	if len(os.Args) < 2 {
+		fmt.Fprintln(os.Stderr, "usage: lem-in <input-file>")
+		os.Exit(1)
+	}
 
-    path := os.Args[1]
-    f, err := os.Open(path)
-    if err != nil {
-        fmt.Fprintln(os.Stderr, err)
-        os.Exit(1)
-    }
-    defer f.Close()
+	path := os.Args[1]
+	f, err := os.Open(path)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	defer f.Close()
 
-    if _, err := io.Copy(os.Stdout, f); err != nil {
-        fmt.Fprintln(os.Stderr, err)
-        os.Exit(1)
-    }
+	if _, err := io.Copy(os.Stdout, f); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
 }
